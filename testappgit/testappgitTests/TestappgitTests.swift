7b060bb32fd60bdb0174b6123babdb6f1a7a252b
Senthil Kumar TD <senthil.kumar@photoninfotech.net>
2019-08-01 16:00:01 +0530

    testsddd

--- testappgit/testappgitTests/TestappgitTests.swift
+++ testappgit/testappgitTests/TestappgitTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import testappgit
 
-class testappgitTests: XCTestCase {
+class TestappgitTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- testappgit/testappgitUITests/TestappgitUITests.swift
+++ testappgit/testappgitUITests/TestappgitUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class testappgitUITests: XCTestCase {
+class TestappgitUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
